101a73a5f7dd459c5a7fdfc8f1364fb9c0b707fc
Arnaud DRAZEK <no@codekraft.local>
2018-05-07 16:02:35 +0200

    Managing errors server side

--- app/routes.py
+++ app/routes.py
@@ -4,24 +4,35 @@ from app import prenoms
 from flask import request
 from flask import json
 
+print("--> Reading first names file from 1900 to 2015 (takes time).")
 d = prenoms.read_prenom_file('assets/dpt2016.txt')
+print("--> Done!")
 
 @app.route('/')
 @app.route('/index')
 def index():
-
-    print("--> Reading first names file from 1900 to 2015 (takes time).")
     
     prenom = request.args.get('firstname')
     depuis = 1900
     sexe = request.args.get('sex')
 
     print("--> Scoring {}".format('Arnaud'))
-    p = prenoms.score(d, prenom, sexe, depuis).to_json(orient='table')
+    error = None
+    try:
+        p = prenoms.score(d, prenom, sexe, depuis).to_json(orient='table')
+    except Exception as inst:
+        error = inst
 		
-    response = app.response_class(
-        response=json.dumps(p),
-        status=200,
-        mimetype='application/json'
-    )
+    if error == None:
+        response = app.response_class(
+            response=json.dumps(p),
+            status=200,
+            mimetype='application/json'
+        )
+    else:
+        response = app.response_class(
+            response="{error: '" + repr(error) + "'}",
+            status=500,
+            mimetype='application/json'
+        )
     return response
